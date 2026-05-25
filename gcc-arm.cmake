abfa4b94ee49438edad867f1b73e7cc77f292afc
whitebelyash <whbexiumwork@gmail.com>
2026-05-26 02:02:36 +0400

    Enable exceptions on arm toolchain (attempt)
    Signed-off-by: whitebelyash <whbexiumwork@gmail.com>

--- gcc-arm.cmake
+++ gcc-arm.cmake
@@ -1,13 +1,15 @@
-SET(CMAKE_SYSTEM_NAME Linux)
-SET(CMAKE_SYSTEM_PROCESSOR arm)
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR arm)
 
 set(CROSS_PATH "/opt/gcc-android")
 
-SET(CMAKE_C_COMPILER "${CROSS_PATH}/bin/arm-linux-androideabi-gcc")
-SET(CMAKE_CXX_COMPILER "${CROSS_PATH}/bin/arm-linux-androideabi-g++")
+set(CMAKE_C_COMPILER "${CROSS_PATH}/bin/arm-linux-androideabi-gcc")
+set(CMAKE_CXX_COMPILER "${CROSS_PATH}/bin/arm-linux-androideabi-g++")
 
-SET(CMAKE_FIND_ROOT_PATH "${CROSS_PATH}/sysroot")
+set(CMAKE_FIND_ROOT_PATH "${CROSS_PATH}/sysroot")
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+add_compile_options(-fexceptions)
